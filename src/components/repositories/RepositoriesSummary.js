d4ffca56f713fb9021432bb2acb319c583f9bc94
Tomek Rychtyk <tomek@rychtyk.pl>
2023-05-11 00:52:10 +0200

    WIP

--- src/components/repositories/RepositoriesSummary.js
+++ src/components/repositories/RepositoriesSummary.js
@@ -1,15 +1,16 @@
 import { StarIcon } from '@primer/octicons-react';
 
 function RepositoriesSummary({ repository }) {
-  const { stargazers_count, open_issues, forks } = repository;
+  const { stargazers_count, open_issues, forks, language } = repository;
 
   return (
-    <div className="flex flex-row gap-4 text-gray-700">
+    <div className='flex flex-row gap-4 text-gray-700'>
       <div>
-        <StarIcon aria-label="stars" size={16} /> {stargazers_count}
+        <StarIcon aria-label='stars' size={16} /> {stargazers_count}
       </div>
       <div>{open_issues} issues need help</div>
       <div>{forks} Forks</div>
+      <div>{language}</div>
     </div>
   );
 }
---
+++ src/components/repositories/RepositoriesSummary.test.js
@@ -0,0 +1,20 @@
+import { screen, render } from '@testing-library/react';
+import RepositoriesSummary from './RepositoriesSummary';
+
+test('display information about the repository', () => {
+  const repository = {
+    language: 'Javascript',
+    stargazers_count: 5,
+    forks: 30,
+    open_issues: 23,
+  };
+
+  render(<RepositoriesSummary repository={repository} />);
+
+  for (let key in repository) {
+    const value = repository[key];
+    const element = screen.getByText(new RegExp(value));
+
+    expect(element).toBeInTheDocument();
+  }
+});
